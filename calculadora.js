cdc867a4b352767da9b12a7cb1ac4a61d187ad80
unknown <2214290009@academico.iesb.br>
2023-05-11 20:30:38 -0300

    criação da calculadora, e ambiente de teste

---
+++ calculadora.js
@@ -0,0 +1,21 @@
+function somar(a,b){
+    return a+b
+}
+//console.log('A soma de 2+9: ', somar(2,9))
+exports.somar = somar
+
+function multiplicar(a,b){
+    return a*b
+}
+//console.log('5 multiplicado por 2 é ',multiplicar(5,2))
+exports.multiplicar = multiplicar
+
+function subtrair(a,b){
+    return a-b
+}
+exports.subtrair = subtrair
+
+function divisao(a,b) {
+    return a/b
+}
+exports.divisao = divisao
---
+++ calculadora.test.js
@@ -0,0 +1,15 @@
+const calculadora = require('./calculadora')
+describe('Testando a calculadora', () =>{
+    test('1+2 = 3', () => {
+        expect(calculadora.somar(1,2)).toBe(3)
+    })
+    test('5*2 = 10', () => {
+        expect(calculadora.multiplicar(5,2)).toEqual(10)
+    })
+    test('2-5 <> 3', () => {
+        expect(calculadora.subtrair(2,5)).not.toBe(3)
+    })
+    test('5/3 ≃ 1.7', () => {
+        expect(calculadora.divisao(5,3)).toBeCloseTo(1.7, 1)
+    })
+})                      
